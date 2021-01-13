6e8365062e54d307c475176dabfd65c36b0d717d
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-01-12 23:19:39 -0600

    Add js implementation using fillRect

--- index.js
+++ index.js
@@ -5,10 +5,24 @@ async function main() {
   const width = 600;
   const height = 600;
 
+  // WASM implementation
   const canvas = document.getElementById("demo-canvas");
   canvas.width = width;
   canvas.height = height;
+  const ctx = canvas.getContext("2d");
 
+  draw_wasm(instance, ctx, width, height)
+
+  // JS implementation using fillRect
+  const canvas2 = document.getElementById("demo-canvas-2");
+  canvas2.width = width
+  canvas2.height = height
+  const ctx2 = canvas2.getContext("2d")
+
+  draw_js(ctx2, width, height)
+}
+
+function draw_wasm(instance, ctx, width, height) {
   const buffer_address = instance.exports.BUFFER.value; // here
   const image = new ImageData(
       new Uint8ClampedArray(
@@ -19,11 +33,17 @@ async function main() {
       width,
   );
 
-  const ctx = canvas.getContext("2d");
-
   instance.exports.go(); // here
 
   ctx.putImageData(image, 0, 0);
+}
+
+function draw_js(ctx2, width, height) {
+  ctx2.fillStyle = 'blue'
+  ctx2.fillRect(0, 0, width/2, height)
+
+  ctx2.fillStyle = 'red'
+  ctx2.fillRect(width/2, 0, width/2, height)
 }
 
 main().catch((err) => {throw err})
